3af2de8eac096235256e341e4e1d202fb011814b
AlbertGirones <agirones600@gmail.com>
2023-02-14 23:33:25 +0100

    BDD & users files

--- bdd/infobdn (1).sql
+++ bdd/infobdn (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-10-2022 a las 10:06:33
+-- Tiempo de generación: 14-02-2023 a las 23:31:01
 -- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Versión de PHP: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,7 +39,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`user`, `passwd`) VALUES
-('admin', '21232f297a57a5a743894a0e4a801fc3');
+('admin', '21232f297a57a5a743894a0e4a801fc3'),
+('administrador', '202cb962ac59075b964b07152d234b70');
 
 -- --------------------------------------------------------
 
@@ -64,7 +65,10 @@ CREATE TABLE `alumnes` (
 INSERT INTO `alumnes` (`DNI`, `nom_alum`, `cog_alum`, `edat_alum`, `foto_alum`, `correo_alum`, `passwd_alum`) VALUES
 ('451283644Ñ', 'Julian', 'Alvarez Moñon', 19, 'alumimg/451283644Ñ.jpg', 'julian888n@infobdn.cat', 'fc8bb50f37f8198b37ec747fdcf88cbf'),
 ('47334363H', 'Albert', 'Girones', 20, 'alumimg/47334363H.jpg', 'agirones600@gmail.com', '202cb962ac59075b964b07152d234b70'),
-('48133467X', 'Gerard', 'Bejarano Alonso', 22, 'alumimg/48133467X.jpg', 'geriteri@infobdn.cat', 'fc8bb50f37f8198b37ec747fdcf88cbf');
+('48133467X', 'Gerard', 'Bejarano Alonso', 22, 'alumimg/48133467X.jpg', 'geriteri@infobdn.cat', 'fc8bb50f37f8198b37ec747fdcf88cbf'),
+('565656556S', 'Prova', 'Prova', 23, 'alumimg/565656556S.jpg', 'prova@gmail.com', '202cb962ac59075b964b07152d234b70'),
+('77777777L', 'Morad', 'lal', 22, 'img/alumimg/77777777L.jpg', 'morad@gmail.com', '202cb962ac59075b964b07152d234b70'),
+('85293741P', 'Julian', 'Alvarez', 20, 'alumimg/85293741P.jpg', 'julialva@infobdn.cat', '202cb962ac59075b964b07152d234b70');
 
 -- --------------------------------------------------------
 
@@ -89,9 +93,12 @@ CREATE TABLE `cursos` (
 
 INSERT INTO `cursos` (`codi_curs`, `nom_curs`, `desc_curs`, `hores_curs`, `ini_curs`, `fin_curs`, `DNI_prof`, `visible`) VALUES
 (3, 'ASIX', 'asiiiidid', 962, '2023-01-01', '2024-09-20', '741852963K', 1),
-(4, 'DAW', 'asiiiididsstrtrr', 743, '2023-01-20', '2024-02-26', '47334363H', 1),
+(4, 'DAW', 'asiiiididsstrtrr', 7444, '0000-00-00', '0000-00-00', '47334363H', 1),
 (5, 'MANTENIMENT', 'Hardware', 546, '2022-12-03', '2023-01-01', '672996313Y', 1),
-(6, 'PHP', 'Projectes en PHP', 400, '2022-10-19', '2022-10-29', '852162124O', 1);
+(6, 'PHP', 'Projectes en PHP', 400, '2023-10-10', '2023-10-29', '852162124O', 1),
+(7, 'JAVA', 'sasa', 60, '2023-02-14', '2023-03-03', '797979797P', 0),
+(8, 'dssddsdsd', 'sdsdsd', 4, '2023-02-22', '2023-03-05', '852162124O', 0),
+(9, 'ProvaFinal', 'Curs de la prova de foc', 100, '2023-02-21', '2023-02-28', '797979797P', 1);
 
 -- --------------------------------------------------------
 
@@ -115,7 +122,12 @@ INSERT INTO `matricula` (`DNI_alum`, `curso`, `nota`) VALUES
 ('47334363H', 3, 0),
 ('47334363H', 5, 6),
 ('451283644Ñ', 4, 0),
-('451283644Ñ', 5, 0);
+('451283644Ñ', 5, 8),
+('85293741P', 3, 0),
+('85293741P', 4, 0),
+('85293741P', 5, 0),
+('77777777L', 6, 0),
+('77777777L', 9, 5);
 
 -- --------------------------------------------------------
 
@@ -138,10 +150,12 @@ CREATE TABLE `professors` (
 --
 
 INSERT INTO `professors` (`DNI`, `nom_prof`, `cog_prof`, `titol_prof`, `foto_prof`, `visible`, `passwd_prof`) VALUES
-('47334363H', 'Lluis', 'Lopez Ruiz', 'Matematic', 'profesimg/47334363H.jpg', 0, 'ac8c0826d26ace7d67486fa3ef3c0290'),
-('672996313Y', 'Oriol', 'Taradellas', 'Técnic en hardware', 'profesimg/672996313Y.jpg', 1, '202cb962ac59075b964b07152d234b70'),
-('741852963K', 'Marcos', 'Alonso', 'Licenciado en educación infantil', 'profesimg/741852963K.jpg', 1, 'ac8c0826d26ace7d67486fa3ef3c0290'),
-('852162124O', 'Maria', 'Angelita', 'Master de dades', 'profesimg/852162124O.jpg', 1, '202cb962ac59075b964b07152d234b70');
+('47334363H', 'Lluis', 'Lopez Ruizss', 'Matematica', '', 0, 'ac8c0826d26ace7d67486fa3ef3c0290'),
+('672996313Y', 'Oriol', 'Taradellas', 'Técnic en hardware', 'img/profesimg/672996313Y.jpg', 1, '202cb962ac59075b964b07152d234b70'),
+('741852963K', 'Marcos', 'Alonso', 'Licenciado en educaciósssn infantil', 'img/profesimg/741852963K.jpg', 1, 'ac8c0826d26ace7d67486fa3ef3c0290'),
+('797979797P', 'Albertinho', 'GR', 'Es oro', 'img/profesimg/797979797P.jpg', 1, '202cb962ac59075b964b07152d234b70'),
+('852162124O', 'Maria', 'Angelita', 'Master de dades', 'img/profesimg/852162124O.jpg', 1, '202cb962ac59075b964b07152d234b70'),
+('dsdsdsd', 'sdsds', 'dsdsds', 'sdsds', 'img/profesimg/dsdsdsd.jpg', 0, '202cb962ac59075b964b07152d234b70');
 
 --
 -- Índices para tablas volcadas
@@ -182,7 +196,7 @@ ALTER TABLE `professors`
 -- AUTO_INCREMENT de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `codi_curs` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `codi_curs` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restricciones para tablas volcadas
