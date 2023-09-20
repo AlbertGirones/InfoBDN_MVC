b9db47636d1ca19d7413d61c10ebb9318ee30a26
Albert Girones <agirones600@gmail.com>
2023-09-20 13:45:56 +0200

    dsdsds

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function obrirFinestraConcurs() {
-    var altura=600;
-    var amplada=700;
+    var altura=400;
+    var amplada=500;
     var x = parseInt((window.screen.height/2)-(altura/2));
     var y = parseInt((window.screen.width/2)-(amplada/2));
     window.open("views/alumne/hla.php","La meva finestra","height="+altura+",width="+amplada+",top="+x+",left="+y+"");
