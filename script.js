9d155142d443e0c801a91b561247094c18423350
Gerard <gebeal2002@gmail.com>
2023-09-20 13:52:56 +0200

    dfghjkl

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function obrirFinestraConcurs() {
-    var altura=400;
-    var amplada=500;
+    var altura=350;
+    var amplada=450;
     var x = parseInt((window.screen.height/2)-(altura/2));
     var y = parseInt((window.screen.width/2)-(amplada/2));
     window.open("views/alumne/hla.php","La meva finestra","height="+altura+",width="+amplada+",top="+x+",left="+y+"");
