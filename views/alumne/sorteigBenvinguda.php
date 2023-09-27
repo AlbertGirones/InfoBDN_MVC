18bf1981a5f692ad2ecf7ddcf5c4724942481d95
Gerard <gebeal2002@gmail.com>
2023-09-27 12:57:15 +0200

    Finestra Maquetada

--- views/alumne/sorteigBenvinguda.php
+++ views/alumne/sorteigBenvinguda.php
@@ -4,51 +4,16 @@
     <title>Sorteig de Benvinguda</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="../../script.js"></script>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
-            margin: 0;
-            padding: 0;
-        }
-
-        h1 {
-            text-align: center;
-            margin-top: 20px;
-        }
-
-        #resultat {
-            text-align: center;
-            margin-top: 20px;
-            padding: 20px;
-            background-color: #ffffff;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        }
-
-        button {
-            display: block;
-            margin: 0 auto;
-            padding: 10px 20px;
-            background-color: #007bff;
-            color: #ffffff;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        button:hover {
-            background-color: #0056b3;
-        }
-    </style>
+    <link rel="stylesheet" href="../../style.css">
 </head>
 
-<body>
-    <h1>Enhorabona!</h1>
+<body class="bodySorteig">
+    <h1 class="titol">Enhorabona!</h1>
     <div id="resultat"></div>
     <script>
         var premiAleat = obtenirPremiAleat();
         document.getElementById("resultat").innerHTML = "<h2>Has guanyat:</h2><p>" + premiAleat + "</p>";
     </script>
+    <a onclick='window.close()'><img src="../../img/48/icons8-cross-mark-48.png" class="imatgeCross"></a>
 </body>
 </html>
